e3e2649be0b87f18500f0f9b2551f7a8b9205e22
Tom Ivar Helbekkmo <tih@hamartun.priv.no>
2021-08-09 19:09:14 +0200

    Start getting links working

--- ioproc/server/sun/sunlocal.c
+++ ioproc/server/sun/sunlocal.c
@@ -2393,7 +2393,7 @@ ObjInfo *Heliosinfo;
     strncpy(Heliosinfo->DirEntry.Name, ++p, 31);
   }
 
-  Debug (FileIO_Flag, ("XXXX object %s is of type %x",
+  Debug (FileIO_Flag, ("XXXX object %s is of type %lx",
 		       name, searchbuffer.st_mode & S_IFMT));
 
   switch(searchbuffer.st_mode & S_IFMT)
